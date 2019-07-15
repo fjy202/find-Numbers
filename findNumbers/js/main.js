631170c021c4ac76ef2294489b7d3b5bab964b2b
Your Name <you@example.com>
2019-07-15 09:45:45 +0800

    edited images

--- findNumbers/js/main.js
+++ findNumbers/js/main.js
@@ -1,1 +1,18 @@
-﻿// 在此放置代码!
+﻿pressd=[]
+for (var i = 0; i < document.body.getElementsByClassName("number").length; i++) {
+    n = document.body.getElementsByClassName("number")[i]
+    n.style = "top:" + Math.floor(Math.random() * document.body.scrollHeight) + "px;left:" + Math.floor(Math.random() * document.body.scrollWidth) + "px;";
+    n.onclick = function () {
+        pressd.push(this.innerText)
+        this.hidden = true
+        if (pressd.length === 10) {
+            a = Windows.UI.Popups.MessageDialog("你赢了！关闭窗口后按alt+r再玩一遍。", "你赢了")
+            a.showAsync()
+            q = document.createElement("a")
+            q.href = "/index.html"
+            q.accessKey = "r"
+            q.onclick = function () { console.log(1); location.reload() }
+            document.body.append(q)
+        }
+    }
+}
